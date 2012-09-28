0c3aa5a390420b26b9ab7f2e3a83b46932c72c8f
Dustin Meijer <du.a.meijer@googlemail.com>
2012-09-28 21:31:18 +0200

    XML Parsing update

--- src/logic/ParserCorrector.java
+++ src/logic/ParserCorrector.java
@@ -2,8 +2,11 @@ package logic;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.List;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
@@ -53,8 +56,51 @@ public class ParserCorrector implements Runnable{
 		// TODO Auto-generated method stub
 		System.out.println("ParserCorrector : Correcting.");
 		
+		Element weatherData = xmlDoc.getRootElement();
 		
-	}
+		System.out.println(weatherData.getName());
+		
+		List<Element> measurements = weatherData.getChildren();
+		List<Element> elements = measurements.get(1).getChildren();
+		System.out.println(elements.size());
+		System.out.println(elements.get(0).getName());
+		
+		
+		/*
+		for(Element measurement : weatherData.getChildren()){
+			List<Attribute> l = measurement.getAttributes();
+			for(Attribute a : l){
+				System.out.println(a.getName());
+			}*/
+			/*
+			System.out.println("Stn : " + measurement.getAttributeValue("STN"));
+			System.out.println("Date : " + measurement.getAttributeValue("date"));
+			System.out.println("Time : " + measurement.getAttributeValue("time"));
+			System.out.println("Temp : " + measurement.getAttributeValue("temp"));
+			System.out.println("Dewp : " + measurement.getAttributeValue("dewp"));
+			System.out.println("Stp : " + measurement.getAttributeValue("stp"));
+			System.out.println("Visib : " + measurement.getAttributeValue("visib"));
+			System.out.println("Wdsp : " + measurement.getAttributeValue("wdsp"));
+			System.out.println("Prcp : " + measurement.getAttributeValue("prcp"));
+			System.out.println("Sndp : " + measurement.getAttributeValue("sndp"));
+			System.out.println("Frshtt : " + measurement.getAttributeValue("frshtt"));
+			System.out.println("Cldc : " + measurement.getAttributeValue("cldc"));
+			System.out.println("Wnddir : " + measurement.getAttributeValue("wnddir"));
+			*/
+			
+		}
+		/*
+		Element root = antbuild.getRootElement();
+		String deftarget = root.getAttributeValue("default", "all");
+		for (Element target : root.getChildren("target")) {
+    		if (deftarget.equals(element.getAttributeValue("name"))) {
+        		System.out.println("The default target " + deftarget + 
+                " has dependencies " + target.getAttributeValue("depends"));
+    		}
+		}
+		 
+		
+	}*/
 	
 
 	private void save() {
