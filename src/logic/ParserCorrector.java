e5c2f7fb04d214d4e34e5ec9b5a0ed2a5e602049
Dustin Meijer <du.a.meijer@googlemail.com>
2012-10-01 14:19:57 +0200

    First Save attempt

--- src/logic/ParserCorrector.java
+++ src/logic/ParserCorrector.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.List;
 
-import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
-import org.jdom2.Namespace;
 import org.jdom2.input.SAXBuilder;
 
 import database.DBPortal;
+import domain.Measurement;
 
 public class ParserCorrector implements Runnable{
 
@@ -21,7 +20,7 @@ public class ParserCorrector implements Runnable{
 	private Document xmlDoc;
 	private SAXBuilder builder;
 	
-	private DBPortal db;
+	private Measurement measurement;
 	
 	public ParserCorrector(StringBuffer input){
 		this.input = input;
@@ -89,24 +88,67 @@ public class ParserCorrector implements Runnable{
 		// Start van for-loop
 		List<Element> elements = measurements.get(1).getChildren();
 		
-		for(Element measurement : measurements){
-			System.out.println("Name : " + measurement.getName());
-			System.out.println("Value :" + measurement.getValue());
-			
-			System.out.println("Stn : " + measurement.getAttributeValue("STN"));
-			System.out.println("Date : " + measurement.getAttributeValue("date"));
-			System.out.println("Time : " + measurement.getAttributeValue("time"));
-			System.out.println("Temp : " + measurement.getAttributeValue("temp"));
-			System.out.println("Dewp : " + measurement.getAttributeValue("dewp"));
-			System.out.println("Stp : " + measurement.getAttributeValue("stp"));
-			System.out.println("Visib : " + measurement.getAttributeValue("visib"));
-			System.out.println("Wdsp : " + measurement.getAttributeValue("wdsp"));
-			System.out.println("Prcp : " + measurement.getAttributeValue("prcp"));
-			System.out.println("Sndp : " + measurement.getAttributeValue("sndp"));
-			System.out.println("Frshtt : " + measurement.getAttributeValue("frshtt"));
-			System.out.println("Cldc : " + measurement.getAttributeValue("cldc"));
-			System.out.println("Wnddir : " + measurement.getAttributeValue("wnddir"));
+		int stn = 0;
+		String date = "";
+		String time = "";
+		float temp = 0;
+		float dewp = 0;
+		float stp = 0;
+		float slp = 0;
+		float visib = 0;
+		float wdsp = 0;
+		float prcp = 0;
+		float sndp = 0;
+		byte frshtt = 0;
+		float cldc = 0;
+		short wnddir = 0;
+		
+		for(Element e : elements){
+			System.out.println("Name : " + e.getName());
+			System.out.println("Value :" + e.getValue());
+
+			if(e.getName().equals("STN")){
+				stn = new Integer(e.getValue()).intValue();
+			}
+			if(e.getName().equals("DATE")){
+				date = e.getValue();
+			}
+			if(e.getName().equals("TIME")){
+				time = e.getValue();
+			}
+			if(e.getName().equals("TEMP")){
+				temp = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("DEWP")){
+				dewp = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("STP")){
+				stp = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("SLP")){
+				slp = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("VISIB")){
+				visib = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("WDSP")){
+				wdsp = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("PRCP")){
+				prcp = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("FRSHTT")){
+				sndp = new Float(e.getValue()).floatValue();
+			}
+			if(e.getName().equals("CLDC")){
+				frshtt = new Byte(e.getValue()).byteValue();
+			}
+			if(e.getName().equals("WNDDIR")){
+				wnddir = new Short(e.getValue()).shortValue();
+			}
 		}
+		
+		this.measurement = new Measurement(stn, date, time, temp, dewp, stp, slp, visib, wdsp, prcp, sndp, frshtt, cldc, wnddir);
 		
 		
 		/*
@@ -145,12 +187,14 @@ public class ParserCorrector implements Runnable{
 		
 	}*/
 	
+	
 
 	private void save() {
 		// TODO Auto-generated method stub
 		System.out.println("ParserCorrector : Saving.");
 		
-
+		new DBPortal(measurement).saveWeatherData();
+		
 		//this.db = new DBPortal(0, null, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 	}
 
